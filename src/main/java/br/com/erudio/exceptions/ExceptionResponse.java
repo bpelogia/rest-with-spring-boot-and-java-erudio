d73c8e0922567abbff5f257e960d6f7fae3127b8
Bruno Pelogia <bruno.pelogia@zflow.com.br>
2022-12-08 03:53:48 -0300

    calculator api

---
+++ src/main/java/br/com/erudio/exceptions/ExceptionResponse.java
@@ -0,0 +1,74 @@
+package br.com.erudio.exceptions;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class ExceptionResponse implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Date timeStamp;
+    private String message;
+    private String details;
+
+    public ExceptionResponse(Date timeStamp, String message, String details) {
+        this.timeStamp = timeStamp;
+        this.message = message;
+        this.details = details;
+    }
+
+
+    /**
+     * get field
+     *
+     * @return timeStamp
+     */
+    public Date getTimeStamp() {
+        return this.timeStamp;
+    }
+
+    /**
+     * set field
+     *
+     * @param timeStamp
+     */
+    public void setTimeStamp(Date timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+
+    /**
+     * get field
+     *
+     * @return message
+     */
+    public String getMessage() {
+        return this.message;
+    }
+
+    /**
+     * set field
+     *
+     * @param message
+     */
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    /**
+     * get field
+     *
+     * @return details
+     */
+    public String getDetails() {
+        return this.details;
+    }
+
+    /**
+     * set field
+     *
+     * @param details
+     */
+    public void setDetails(String details) {
+        this.details = details;
+    }
+}
